51dd1883eb4b30f725e449c14cef69a8a007e832
Vjy524 <vijay@wielabs.com>
2026-08-06 13:06:36 +0530

    Stop tracking constants.dart

--- lib/utils/constants/constants.dart
+++
@@ -1,8 +0,0 @@
-class Constants {
-  static const String supabaseUrl = 'https://vubwgaaouklwmybpmcxh.supabase.co';
-  // static const String supabaseUrl =
-  //     'https://vubwgaaouklwmybpmcxh.supabase.co/rest/v1/';
-
-  static const String supabaseKey =
-      'sb_publishable_hENFI-jqtLB2bKmvCPOCWA_27XkuWY2';
-}
